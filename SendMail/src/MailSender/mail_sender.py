c57cf981633b40974678caf6232486278bfa4a34
Ivan Chen <ivanchen@eurofins.com>
2020-01-31 22:12:47 +0800

    commit send mail project

---
+++ SendMail/src/MailSender/mail_sender.py
@@ -0,0 +1,88 @@
+'''To send mail'''
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import email
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+import smtplib
+
+
+class SendMail:
+    '''to send mail'''
+
+    def __init__(self, From, To, Cc, pw, file_path, file_header, file_body):
+        # 发送人
+        self.from_list = From
+        # 收件人['aaa@a.com','bbb@a.com']
+        self.to_list = list(To)
+        # 抄送人
+        self.copy_list = list(Cc)
+        # 登录邮件密码base64.encodestring('明文')加密后密码
+        self.pass_word = pw
+        # 文件具体路径(路径+文件名称)
+        self.file_path = file_path
+        # 标题头
+        self.file_header = file_header
+        # 内容
+        self.file_body = file_body
+
+    def send(self):
+        '''send mail'''
+        server = smtplib.SMTP('smtp.qq.com')
+        server.starttls()
+        # pwd = base64.decodestring(self.pw)
+        server.login(self.from_list, self.pass_word)
+        try:
+            server.sendmail(self.from_list, self.to_list, self.atta())
+        finally:
+            server.quit()
+
+    def atta(self):
+        '''attach attachment'''
+        main_msg = MIMEMultipart()
+        # 内容
+        text_msg = MIMEText(self.file_body)
+        main_msg.attach(text_msg)
+        try:
+            contype = 'application/octet-stream'
+            maintype, subtype = contype.split('/', 1)
+
+            data = open(self.file_path, 'rb')
+            file_msg = MIMEBase(maintype, subtype)
+            file_msg.set_payload(data.read())
+            data.close()
+            email.encoders.encode_base64(file_msg)
+            basename = os.path.basename(self.file_path.split('/')[-1])
+            file_msg.add_header('Content-Disposition',
+                                'attachment', filename=basename)
+            main_msg.attach(file_msg)
+        except Exception as err_exception:
+            print(err_exception)
+
+        main_msg['From'] = self.from_list
+        main_msg['To'] = ";".join(self.to_list)
+        main_msg['Cc'] = ";".join(self.copy_list)
+
+        # 标题头
+        main_msg['Subject'] = self.file_header
+        main_msg['Date'] = email.utils.formatdate()
+
+        full_text = main_msg.as_string()
+
+        return full_text
+
+
+if __name__ == '__main__':
+    MAIL_PASS = "xxmyrwzexgiibdhb"  # 密码(这里的密码不是登录邮箱密码，而是授权码)
+    CONTENT = 'Python Send Mail !'
+    TITLE = 'Python SMTP Mail Test'  # 邮件主题
+    ATTACHMENT = r"D:\GitGUI\SeleniumPythonFramework\src\TestResult\0130_221636\Plan1\Plan1.html"
+    SENDER = '616191459@qq.com'  # 发件人邮箱
+    RECEIVERS = ['616191459@qq.com']  # 接收人邮箱
+    S = SendMail(SENDER, RECEIVERS, [], MAIL_PASS, ATTACHMENT, TITLE, CONTENT)
+    S.send()
+    print('发送成功！')
